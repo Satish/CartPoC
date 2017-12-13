f50cdb4ebc8985b6fd707fe91ce40755203ba9c2
Satish Chauhan <satishchauhan80@gmail.com>
2017-12-14 02:00:36 +0530

    first commit

--- Cart/CartViewController.swift
+++ Cart/CartViewController.swift
@@ -10,26 +10,78 @@ import UIKit
 
 class CartViewController: UIViewController {
 
+    @IBOutlet private weak var overlay: UIView! {
+        didSet {
+            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(CartViewController.hideCartDetail(sender:)))
+            overlay.addGestureRecognizer(tapGestureRecognizer)
+        }
+    }
+    @IBOutlet private weak var cartSummaryView: UIView!
+    @IBOutlet private weak var cartDatilView: UIView!
+    
+    @IBOutlet private weak var cartDetailViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var cartSummaryViewHeightConstraint: NSLayoutConstraint!
+
+    weak var delegate: UIViewController?
+    var isVisible: Bool = false
+    var isDetailVisible: Bool = false
+    var summaryHeight: CGFloat = 60
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        summaryHeight = cartSummaryViewHeightConstraint.constant + 10
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func hideCartDetail(sender: Any?) {
+        hideCartDetail()
     }
+
+    func hideCartDetail(sender: Any?, completion: @escaping () -> Void) {
+        hideCartDetail(completion: completion)
+    }
+
+    @IBAction private func toggleCartDetail(sender: Any?) {
+        if isDetailVisible {
+            hideCartDetail()
+        } else {
+            showCartDetail()
+        }
+    }
+
+    private func showCartDetail() {
+        if let vc = delegate as? ViewController {
+            overlay.alpha = 0
+            vc.cartContainerViewHeightConstraint.constant = vc.view.frame.height+100 //add buffer
+            vc.cartContainerView.superview?.layoutIfNeeded()
+
+            cartDetailViewHeightConstraint.constant = summaryHeight + 300
+            UIView.animate(withDuration: 0.3, animations: { [weak self] in
+                self?.overlay.alpha = 0.4
+                self?.cartSummaryView.superview?.layoutIfNeeded()
+            }) { [weak self] (_) in
+                self?.isDetailVisible = true
+            }
+        }
+    }
+
+    private func hideCartDetail(completion: (() -> Void)?=nil) {
+        if let vc = delegate as? ViewController {
+            cartDetailViewHeightConstraint.constant = summaryHeight
+
+            UIView.animate(withDuration: 0.3, animations: { [weak self] in
+                self?.overlay.alpha = 0
+                self?.cartSummaryView.superview?.layoutIfNeeded()
+            }) { [weak self] (_) in
+                vc.cartContainerViewHeightConstraint.constant = self?.summaryHeight ?? 0
+                vc.cartContainerView.superview?.layoutIfNeeded()
+                self?.isDetailVisible = false
+
+                completion?()
+            }
+        }
+        
+    }
+
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Cart/ViewController.swift
+++ Cart/ViewController.swift
@@ -10,16 +10,110 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.delegate           = self
+            tableView.dataSource         = self
+            tableView.rowHeight          = UITableViewAutomaticDimension
+            tableView.tableFooterView    = UIView(frame: CGRect.zero)
+            tableView.estimatedRowHeight = 36
+
+        }
+    }
+
+    @IBOutlet weak var cartContainerView: UIView!
+    @IBOutlet weak var cartContainerViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var cartContainerViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var tableViewBottomConstraint: NSLayoutConstraint!
+
+    private var rows = [String]()
+    var cartVC: CartViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        for n in 1...100 {
+            rows.append("Row \(n)")
+        }
+
+        cartContainerViewBottomConstraint.constant = -cartVC.summaryHeight
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+
+        if let vc = segue.destination as? CartViewController {
+            cartVC = vc
+            vc.delegate = self
+        }
     }
 
+    @IBAction private func showCart(sender: Any?) {
+        if cartVC.isVisible {
+            hideCart()
+        } else {
+            showCart()
+        }
+    }
+
+    private func showCart() {
+        cartContainerViewBottomConstraint.constant = 0
+        tableViewBottomConstraint.constant = cartVC.summaryHeight
+
+        UIView.animate(withDuration: 0.3, animations: { [weak self] in
+            self?.cartContainerView.superview?.layoutIfNeeded()
+        }) {  [weak self] (_) in
+            self?.cartVC.isVisible = true
+        }
+    }
+
+    private func hideCart() {
+        func hideCartAnimated() {
+            UIView.animate(withDuration: 0.3, animations: { [weak self] in
+                self?.cartContainerView.superview?.layoutIfNeeded()
+            }) {  [weak self] (_) in
+                self?.cartVC.isVisible = false
+            }
+        }
+
+        cartContainerViewBottomConstraint.constant = -cartVC.summaryHeight
+        tableViewBottomConstraint.constant = 0
+
+        if cartVC.isDetailVisible {
+            cartVC.hideCartDetail(sender: self)
+            hideCartAnimated()
+        } else {
+            hideCartAnimated()
+        }
+    }
+
+    func showCartDetail() {
+        UIView.animate(withDuration: 0.3, animations: { [weak self] in
+            self?.cartContainerView.superview?.layoutIfNeeded()
+        }) {  [weak self] (_) in
+            if let isVisible = self?.cartVC.isVisible {
+                self?.cartVC.isVisible = !isVisible
+            }
+        }
+    }
 
 }
 
+extension ViewController: UITableViewDelegate {
+
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rows.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = rows[indexPath.row]
+
+        return cell
+    }
+
+}
